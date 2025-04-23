8ba8768c6802b9055c4b0d85080beaea1d397736
DIFoundation <adeniranibrahim165@gmail.com>
2025-04-23 15:23:21 +0100

    Getting Nave bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <section className="text-center py-20 px-4">
-      <h2 className="text-4xl font-bold mb-4 text-blue-900">
-        Welcome to Jerry Concept Organisation
-      </h2>
-      <p className="text-gray-700 text-lg max-w-2xl mx-auto">
-        Your one-stop shop for typing, printing, internet access, digital
-        services, and more. Fast, affordable, and reliable.
-      </p>
+    <section className="bg-blue-900 h-screen text-center content-center py-20 px-4">
+      <HeroSection />
+      
     </section>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav className="fixed bg- text-blue-900 px-20 py-6 shadow w-full z-10 ">
+    <nav className="fixed bg-white text-blue-900 px-20 py-6 shadow w-full z-0 ">
       <div className="max-w-screen mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">CyberCenter</h1>
         <div className="space-x-6 text-xl font-semibold">
@@ -13,7 +13,7 @@ export default function Navbar() {
           <Link href="/pricing">Pricing</Link>
           <Link href="/contact">Contact</Link>
         </div>
-        <button href='/booking' className='button'>Book Now</button>
+        <Link href='/booking' className='button bg-blue-900 text-amber-100'>Book Now</Link>
       </div>
     </nav>
   )
